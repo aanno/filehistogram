e900d88b2a8fe82494c02aa8bd204c9359a5e9f9
aanno <aanno@users.noreply.github.com>
2025-06-25 12:22:59 +0200

    add missing src/OsPathInternCache.hs

---
+++ src/OsPathInternCache.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE BangPatterns #-}
+
+module OsPathInternCache
+    ( InternCache
+    , newInternCache
+    , intern
+    , internList
+    , internComponents
+    , cacheSize
+    , cacheStats
+    ) where
+
+import System.OsPath
+import System.OsPath.Types (OsPath)
+import Control.Concurrent.STM
+import qualified Data.HashMap.Strict as HM
+import Data.Hashable (Hashable(..))
+import qualified Data.ByteString.Short as SBS
+import Data.IORef
+import Control.Monad (forM)
+
+-- | Thread-safe interning cache for OsPath values
+data InternCache = InternCache
+    { cache :: TVar (HM.HashMap OsPath OsPath)
+    , stats :: IORef CacheStats
+    }
+
+-- | Statistics for the intern cache
+data CacheStats = CacheStats
+    { lookups :: !Int
+    , hits :: !Int
+    , misses :: !Int
+    } deriving (Show, Eq)
+
+-- | Create a new empty intern cache
+newInternCache :: IO InternCache
+newInternCache = do
+    cacheVar <- newTVarIO HM.empty
+    statsRef <- newIORef (CacheStats 0 0 0)
+    return $ InternCache cacheVar statsRef
+
+-- | Intern a single OsPath value
+-- Returns the canonical instance from the cache
+intern :: InternCache -> OsPath -> IO OsPath
+intern (InternCache cacheVar statsRef) !path = do
+    -- Try to find in cache first (read-only transaction)
+    found <- atomically $ do
+        m <- readTVar cacheVar
+        return $ HM.lookup path m
+    
+    case found of
+        Just canonPath -> do
+            -- Cache hit
+            atomicModifyIORef' statsRef $ \s ->
+                (s { lookups = lookups s + 1, hits = hits s + 1 }, ())
+            return canonPath
+        Nothing -> do
+            -- Cache miss - need to insert
+            atomicModifyIORef' statsRef $ \s ->
+                (s { lookups = lookups s + 1, misses = misses s + 1 }, ())
+            
+            -- Insert into cache (may race with other threads)
+            atomically $ do
+                m <- readTVar cacheVar
+                case HM.lookup path m of
+                    Just canonPath -> 
+                        -- Another thread inserted it
+                        return canonPath
+                    Nothing -> do
+                        -- Insert and return the same instance
+                        writeTVar cacheVar $! HM.insert path path m
+                        return path
+
+-- | Intern a list of OsPath values
+internList :: InternCache -> [OsPath] -> IO [OsPath]
+internList cache paths = mapM (intern cache) paths
+
+-- | Intern path components (useful after splitPath)
+-- This is optimized for the common case where many paths share prefixes
+internComponents :: InternCache -> [OsPath] -> IO [OsPath]
+internComponents cache = internList cache
+
+-- | Get the current size of the cache
+cacheSize :: InternCache -> IO Int
+cacheSize (InternCache cacheVar _) = 
+    HM.size <$> readTVarIO cacheVar
+
+-- | Get cache statistics
+cacheStats :: InternCache -> IO CacheStats
+cacheStats (InternCache _ statsRef) = readIORef statsRef
